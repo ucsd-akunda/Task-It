fae49811a59b0fbdf4e972a15ed85029d00d108a
Ashok Kunda <Ashok Kunda>
2016-11-03 18:13:06 -0700

    made profile page to make more people add to json file -- profile page also looks bad rn

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var add = require('./routes/add');
 var tasks = require('./routes/tasks');
 var groups1 = require('./routes/groups1');
 var feed = require('./routes/feed');
+var profile = require('./routes/profile');
 
 var app = express();
 
@@ -42,6 +43,7 @@ app.get('/add', add.addTask);
 
 app.get('/groups1', groups1.view);
 app.get('/feed', feed.view);
+app.get('/profile/:name', profile.personInfo);
 
 // app.get('/groups1', function(req, res, next) {
   // res.render('groups1', data.groupsData);
---
+++ routes/profile.js
@@ -0,0 +1,16 @@
+var data = require('../profiles.json');
+
+exports.personInfo = function(req, res) {
+	console.log("\nAM INSIDE THE INFO FUNCTION\n");
+	var name = req.params.username;
+	var person;
+
+	for (var i = 0; i < data.length; i++) {
+		if (data[i].username == req.params.name) {
+			console.log(i, "\t", data[i]);
+			person = data[i];
+		}
+	}
+
+	res.render('profile', person);
+}
