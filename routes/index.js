228359ff84cea7b7ceafdf13dd6258112baa983a
Nayeli Ruiz <Ruiz.Nayeli09@gmail.com>
2016-11-03 20:29:15 -0700

    Data driven 'Top Friend' task score, fix navbar/jumbotron styles

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,18 @@
 var data = require('../data.json');
 
+var feed = require('../feed.json');
+
+var topPerson = {score: 0};
+
+feed.posts.forEach(function(person){
+	if(person.score > topPerson.score)
+		topPerson = person;
+});
+
+data.indexData.topPerson = topPerson;
+
+console.log(data);
+
 exports.view = function(req, res){
 	res.render('index', data.indexData);
 };
